7a0fa20bc0641a28d6db1297b8f392710ce889a7
Hugo Azevedo <hugo.soares@outlook.com>
2020-04-22 14:21:59 -0300

    Updates

---
+++ precache-manifest.458cbf15ed77b65780c0fde0ec1377fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daa622b6cd16ab8c78774e7ace6c886f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3cafd7de53fc57492702",
+    "url": "/static/css/main.8be872cb.chunk.css"
+  },
+  {
+    "revision": "047697b8246e8020e16b",
+    "url": "/static/js/2.73cf0eb2.chunk.js"
+  },
+  {
+    "revision": "10b1560d8721035799e40ab028210d16",
+    "url": "/static/js/2.73cf0eb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cafd7de53fc57492702",
+    "url": "/static/js/main.48ab7fe4.chunk.js"
+  },
+  {
+    "revision": "aafe854a040f5edb67aa",
+    "url": "/static/js/runtime-main.4ad7ff05.js"
+  }
+]);
